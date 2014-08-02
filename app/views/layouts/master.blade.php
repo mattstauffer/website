f86cd5179ce28d615227a8253ae3e44f5169d209
Matt Stauffer <mstauffer@gmail.com>
2014-08-02 00:40:26 -0400

    Some responsive iPhone specific tweaks

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -56,7 +56,7 @@
 
     <section id="copyright" class="textcenter">
         <div class="boxed">
-            <div class="animated slideInLeft">Laravel is a trademark of Taylor Otwell.<br class="br-mobile--footer"> Copyright &copy; <a href="http://twitter.com/taylorotwell" title="Taylor Otwell" target="_blank">Taylor Otwell</a>.<br class="br-mobile--footer"> Website built with &hearts; <a href="//ikreativ.com" title="iKreativ" target="_blank">iKreativ</a>; updated by <a href="//tighten.co" title="Tighten Co." target="_blank">Tighten Co.</a></div>
+            <div class="animated slideInLeft">Laravel is a trademark of Taylor Otwell.<br class="br-mobile--footer"> Copyright &copy; <a href="http://twitter.com/taylorotwell" title="Taylor Otwell" target="_blank">Taylor Otwell</a>.<br class="br-mobile--footer"> Website built with &hearts; <a href="//ikreativ.com" title="iKreativ" target="_blank">iKreativ</a>; responsive by <a href="//tighten.co" title="Tighten Co." target="_blank">Tighten Co.</a></div>
         </div>
     </section>
 
